d3a663e924bbdec45cbfabe6a1b04db11b68eb16
juliano <juliano0_fsa@hotmail.com>
2021-07-31 22:39:04 -0300

    add availability profile section above hero

--- src/components/feature/index.tsx
+++ src/components/feature/index.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import ImageSection1 from "@/assets/ImageSection1.png";
+import Image from "next/image";
+import { AppConfig } from "@/utils/AppConfig";
 
 export default function Feature() {
-  return <div></div>;
+  return (
+    <main className="bg-brand-background">
+      <section className="drop-shadow">
+        <article className="flex justify-center items-center flex-col-reverse sm:flex-row">
+          <div className="flex flex-col max-w-lg m-8 items-center sm:items-start drop-shadow">
+            <h2 className="text-medium text-gray-title">
+              {AppConfig.section_title}
+            </h2>
+            <span className="my-4 text-gray-description text-center sm:text-left ">
+              {AppConfig.section_description}
+            </span>
+            <span className="underline text-brand-secondary">
+              {AppConfig.section_link_text}
+            </span>
+          </div>
+          <Image src={ImageSection1} />
+        </article>
+      </section>
+    </main>
+  );
 }
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -7,6 +7,7 @@ import { useTheme } from "@material-ui/core/styles";
 import ImageHeader from "@/assets/ImageHeader.png";
 import playButton from "@/assets/playButton.svg";
 import heroBackground from "@/assets/heroBackground.png";
+import profileImage from "@/assets/profileImage.png";
 
 const Hero = () => {
   const theme = useTheme();
@@ -37,30 +38,30 @@ const Hero = () => {
           </li>
         </Navbar>
       </header>
-      <main className="sm:p-20 p-3">
-        <article className="text-white flex justify-between items-center">
-          <div className="mx-4 max-w-xl flex flex-col justify-between">
-            <h1 className="font-medium max-w-md my-2 text-center sm:text-left">
-              {AppConfig.hero_title_primary}
-            </h1>
-            <span className="flex my-2 sm:text-left text-center">
-              {AppConfig.hero_description_primary}
-            </span>
-            <div className="flex items-center my-10 sm:my-2">
-              <Image src={playButton} />
-              <span className="mx-4 buttonlink">{AppConfig.hero_button}</span>
-              <style jsx>
-                {`
-                  .buttonlink {
-                    text-decoration: underline;
-                  }
-                `}
-              </style>
-            </div>
+      <section className="sm:p-20 p-3 text-white flex justify-around items-center">
+        <div className="mx-4 max-w-xl flex flex-col justify-between">
+          <h1 className="font-medium max-w-md my-2 text-center sm:text-left">
+            {AppConfig.hero_title_primary}
+          </h1>
+          <span className="flex my-2 sm:text-left text-center">
+            {AppConfig.hero_description_primary}
+          </span>
+          <div className="flex items-center my-10 sm:my-2">
+            <Image src={playButton} />
+            <span className="mx-4 underline">{AppConfig.hero_button}</span>
           </div>
-          {matches ? <Image src={ImageHeader}></Image> : () => <></>}
-        </article>
-      </main>
+        </div>
+        {matches ? <Image src={ImageHeader}></Image> : () => <></>}
+      </section>
+      <article className="drop-shadow p-6 bg-white text-center sm: text-left flex flex-col text-xs sm:text-sm sm:flex-row items-center justify-center">
+        <Image src={profileImage} />
+        <span className="underline text-brand-secondary font-medium m-2">
+          {AppConfig.care_day_availability_title}
+        </span>
+        <span className="font-medium text-black">
+          {AppConfig.care_day_availability}
+        </span>
+      </article>
     </Background>
   );
 };
